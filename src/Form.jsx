f225483cf8479e5285e879fe9b1e05bd24306e47
Sam Mariassouce <sammariassouce@gmail.com>
2023-06-04 10:46:17 -1000

    final

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
-const Form = (addItem) => {
+const Form = ({ addItem }) => {
   const [newItemName, setNewItemName] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!newItemName) return;
+    if (!newItemName) {
+      toast.error('please provide value');
+      return;
+    }
     addItem(newItemName);
     setNewItemName('');
   };
+
   return (
     <form onSubmit={handleSubmit}>
-      <h4>Grocery bud</h4>
-      <div className="form-control">
+      <h4>grocery bud</h4>
+      <div className='form-control'>
         <input
-          type="text"
-          className="form-input"
+          type='text '
+          className='form-input'
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type="submit" className="btn">
+        <button type='submit' className='btn'>
           add item
         </button>
       </div>
